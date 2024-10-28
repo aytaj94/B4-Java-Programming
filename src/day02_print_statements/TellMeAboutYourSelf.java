a167455d259b13419baa71f369c9ffde4002f4a3
aytajabdullayeva <abdullayevaaytaj94@gmail.com>
2024-10-28 13:07:59 -0400

    Initial Commit

---
+++ src/day02_print_statements/TellMeAboutYourSelf.java
@@ -0,0 +1,10 @@
+package day02_print_statements;
+
+public class TellMeAboutYourSelf{
+    public static void main (String[] args){
+        System.out.println("Feyruz");
+        System.out.println("I am from Azerbaijan");
+        System.out.println("My favorite color is dark green");
+        System.out.println("End the class very late is my hobby");
+    }
+}
